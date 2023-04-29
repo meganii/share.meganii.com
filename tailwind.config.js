8835f9319e750f1f0ac1ca9c23ef6449e0a68520
meganii <yuhei24@gmail.com>
2023-04-29 15:55:26 +0900

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'themes/tailwind/layouts/**/*.html',
+    'layouts/**/*.html',
+    'content/**/*.html',
+    'content/**/*.md',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ja: ["Helvetica Neue", "Arial", "Hiragino Kaku Gothic ProN", "Hiragino Sans", "Meiryo", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
